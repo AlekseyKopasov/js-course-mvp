dfa25aa8ce1fbd181ab257a0a208a6ab36fc979e
Алексей Копасов <alesey.k1@gmail.com>
2025-06-04 23:09:09 +0300

    добавляет пути для гитхаба

--- vite.config.ts
+++ vite.config.ts
@@ -2,47 +2,51 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@app': path.resolve(__dirname, './src/app'),
-      '@processes': path.resolve(__dirname, './src/processes'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@widgets': path.resolve(__dirname, './src/widgets'),
-      '@features': path.resolve(__dirname, './src/features'),
-      '@entities': path.resolve(__dirname, './src/entities'),
-      '@shared': path.resolve(__dirname, './src/shared'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-    },
-  },
-  publicDir: 'public',
-  base: '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-    sourcemap: true,
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-      },
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-        manualChunks: undefined,
+export default defineConfig(({ mode }) => {
+  const isGitHub = mode === 'github';
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '@app': path.resolve(__dirname, './src/app'),
+        '@processes': path.resolve(__dirname, './src/processes'),
+        '@pages': path.resolve(__dirname, './src/pages'),
+        '@widgets': path.resolve(__dirname, './src/widgets'),
+        '@features': path.resolve(__dirname, './src/features'),
+        '@entities': path.resolve(__dirname, './src/entities'),
+        '@shared': path.resolve(__dirname, './src/shared'),
+        '@assets': path.resolve(__dirname, './src/assets'),
       },
     },
-  },
-  server: {
-    port: 8080,
-    open: '/',
-    host: true,
-    fs: {
-      strict: false,
-      allow: ['..'],
+    publicDir: 'public',
+    base: isGitHub ? '/js-course-mvp/' : '/',
+    build: {
+      outDir: 'dist',
+      assetsDir: 'assets',
+      emptyOutDir: true,
+      sourcemap: true,
+      rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, 'index.html'),
+        },
+        output: {
+          assetFileNames: 'assets/[name]-[hash][extname]',
+          chunkFileNames: 'assets/[name]-[hash].js',
+          entryFileNames: 'assets/[name]-[hash].js',
+          manualChunks: undefined,
+        },
+      },
     },
-  },
+    server: {
+      port: 8080,
+      open: '/',
+      host: true,
+      fs: {
+        strict: false,
+        allow: ['..'],
+      },
+    },
+  };
 });
