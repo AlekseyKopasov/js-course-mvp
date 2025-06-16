df6dd114ed6df615e1010b8cb98b672def6fd297
Алексей Копасов <alesey.k1@gmail.com>
2025-06-16 23:48:47 +0300

    исправляет деплой для github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_URL || '/',
+  base: '/js-course-mvp/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
